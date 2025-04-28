e4a07012ddb769c6fc8125259c7c854e1e3afb3e
Alexandre Talamba <talamba.alexandre@gmail.com>
2025-04-28 10:15:06 -0400

    initial commit

--- backend/utilities/call_openai_file.py
+++ backend/utilities/call_openai_file.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key = "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")
+client = OpenAI(api_key = "get out of here you won't know it")
 
 def call_openai(content):
 
